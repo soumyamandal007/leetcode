4d45d804310caeabe8ce479d4a7fd5ee5fa81c4a
Soumya <31356127+soumyamandal007@users.noreply.github.com>
2022-10-20 00:43:04 -0700

    Time: 33 ms (92.39%), Space: 14 MB (20.14%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def multiply(x,n):
+            if x == 0: return 0
+            if n==0: return 1
+            
+            res = multiply(x * x, n // 2)
+            
+            return res * x if n % 2 else res
+            
+        res = multiply(x, abs(n))
+        
+        return res if n >=0 else 1/res
