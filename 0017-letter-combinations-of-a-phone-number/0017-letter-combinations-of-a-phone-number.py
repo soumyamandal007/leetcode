e4b9aaed4c7f6601ee171680d5323ada00fa137c
Soumya <31356127+soumyamandal007@users.noreply.github.com>
2023-06-28 11:28:00 -0700

    Time: 58 ms (7.25%), Space: 16.3 MB (52.98%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -4,16 +4,17 @@ class Solution:
             return []
         letters = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
         comb = []
-        def backtrack(ind , path):
-            if len(path) == len(digits): #here if the path length and digits length are same 
-                comb.append("".join(path))
+        def backtrack (ind,path):
+            if len(path) == len(digits):
+                comb.append(''.join(path))
                 return
-            pos_letters = letters[digits[ind]] # letters according to the index
-            for l in pos_letters:
+            lett = letters[digits[ind]]
+            for l in lett:
                 path.append(l)
-                backtrack(ind + 1, path)
+                backtrack(ind+1,path)
                 path.pop()
-        
+            
         backtrack(0,[])
+        
         return comb
         
