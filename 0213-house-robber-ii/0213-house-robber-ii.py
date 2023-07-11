fdcbce2f87fec17f43abbb487456fda6ad78d670
Soumya <31356127+soumyamandal007@users.noreply.github.com>
2023-07-11 15:00:01 -0700

    Time: 51 ms (39.21%), Space: 16.3 MB (44.24%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
+        if len(nums) == 3:
+            return max(nums[1],nums[0],nums[2])
+        
+        return max(self.robSum(nums[1:]),self.robSum(nums[:-1]))
+        
+    def robSum(self,nums):
+        dp = [0]*(len(nums))
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+
+        for i in range(2,len(nums)):
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+        return dp[len(nums)-1]
+
+
+
+
+
+
+
+
+
+
