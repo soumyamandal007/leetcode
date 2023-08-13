843338c16e10f902b32af8e1485d03a790799fa7
Soumya <31356127+soumyamandal007@users.noreply.github.com>
2023-08-12 21:07:29 -0700

    Time: 37 ms (89.95%), Space: 16.5 MB (17.31%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -21,15 +21,16 @@ class Solution:
                 num += 1
             right -= 1
             
-            for i in range(right,left-1,-1):
-                mat[bottom][i] = num
-                num += 1
-            bottom -= 1
+            if top <= bottom:
+                for i in range(right,left-1,-1):
+                    mat[bottom][i] = num
+                    num += 1
+                bottom -= 1
             
-
-            for i in range(bottom,top - 1, -1):
-                mat[i][left] = num
-                num += 1
-            left += 1
+            if left <= right:
+                for i in range(bottom,top - 1, -1):
+                    mat[i][left] = num
+                    num += 1
+                left += 1
             
         return mat
