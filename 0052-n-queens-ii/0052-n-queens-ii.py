cfeb9edbe2a96999e9b738b15c8e496d2ca0e324
Soumya <31356127+soumyamandal007@users.noreply.github.com>
2023-08-15 00:59:12 -0700

    Time: 221 ms (7.54%), Space: 16.6 MB (8.61%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def placeNQueens(n, board, row, solutions):
+            if row == n:
+                solutions.append(["".join(row) for row in board])
+                return
+
+            for i in range(n):
+                if isSafe(row, i, board):
+                    board[row][i] = 'Q'
+                    placeNQueens(n, board, row + 1, solutions)
+                    board[row][i] = '.'
+
+        def isSafe(i, j, board):
+            for r in range(len(board)):
+                for c in range(len(board)):
+                    if board[r][c] == 'Q' and (i == r or j == c or i + j == r + c or i - j == r - c):
+                        return False
+            return True
+
+        qBoard = [['.' for _ in range(n)] for _ in range(n)]
+        solutions = []
+        placeNQueens(n, qBoard, 0, solutions)
+        return len(solutions)
