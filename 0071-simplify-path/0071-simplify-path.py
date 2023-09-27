c93570d84a7c27706b35d97cbaaed56457af8636
Soumya <31356127+soumyamandal007@users.noreply.github.com>
2023-09-27 00:51:35 -0700

    Time: 42 ms (49.93%), Space: 16.1 MB (97.33%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,7 +1,6 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         path = path.split("/")
-        print(path)
         st = []
         for char in path:
             if char == "." or char == "":
@@ -11,5 +10,4 @@ class Solution:
                     st.pop()
             else:
                 st.append(char)
-        print(st)
         return "/"+"/".join(st)
